04eaeaa53e73a28d80617b1e5d2adf05044cdb6a
Katrine <katrine.lombardo@gmail.com>
2024-08-23 17:01:44 -0400

    [INIT]: Initialised env

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+from typing import List
 import asana
 
-client = asana.Client.access_token('ACCESS_TOKEN')
+
+# Initialise environment
+def initialize_environment() -> List[str]:
+    load_dotenv(find_dotenv())
+    ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+    WORKSPACE_GID = os.getenv("WORKSPACE_GID")
+
+
+client = asana.Client.access_token("ACCESS_TOKEN")
 
 result = client.tasks.create_task(
-    {'workspace': 'WORKSPACE_GID', 'name': 'Sample task', 'assignee': 'me'})
+    {"workspace": "WORKSPACE_GID", "name": "Sample task", "assignee": "me"}
+)
 
 print(result)
