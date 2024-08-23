38c1f81c3d2e03dc028980da0098fd31f76b817e
Katrine <katrine.lombardo@gmail.com>
2024-08-23 17:15:59 -0400

    [INIT]: Initialised Asana client

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import os
 from dotenv import load_dotenv, find_dotenv
-from typing import List
+from typing import List, Tuple
 import asana
+from asana.rest import ApiException
+from pprint import pprint
 
 
-# Initialise environment
-def initialize_environment() -> List[str]:
+def initialize_environment() -> Tuple[str, str]:
     load_dotenv(find_dotenv())
-    ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-    WORKSPACE_GID = os.getenv("WORKSPACE_GID")
+    ACCESS_TOKEN: str = os.getenv("ACCESS_TOKEN", "")
+    WORKSPACE_GID: str = os.getenv("WORKSPACE_GID", "")
+    return ACCESS_TOKEN, WORKSPACE_GID
 
 
-client = asana.Client.access_token("ACCESS_TOKEN")
+def configure_asana(access_token: str) -> asana.UsersApi:
+    configuration = asana.Configuration()
+    configuration.access_token = access_token
+    api_client = asana.ApiClient(configuration)
 
-result = client.tasks.create_task(
-    {"workspace": "WORKSPACE_GID", "name": "Sample task", "assignee": "me"}
-)
+    users_api_instance = asana.UsersApi(api_client)
+    return users_api_instance
 
-print(result)
+
+def main() -> None:
+    ACCESS_TOKEN, WORKSPACE_GID = initialize_environment()
+    users_api_instance = configure_asana(ACCESS_TOKEN)
+
+    try:
+        opts = {}
+        me = users_api_instance.get_user("me", opts)
+        print("Hello world! " + "My name is " + me["name"] + "!")
+
+    except ApiException as e:
+        print("Exception when calling UsersApi->get_user: %s\n" % e)
+
+
+if __name__ == "__main__":
+    main()
